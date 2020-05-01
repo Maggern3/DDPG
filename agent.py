Fix dimensionality bug in sampling, add batch normalization for both actor & critic, increase buffer size, reduce gamma and noise sigma. Noise changed to be different for each agent. Changed training schedule.

agent.py
@@ -10,27 +10,33 @@ import copy
 class Agent():
     def __init__(self, state_size, action_size):
         super().__init__() 
-        self.actor = Actor(state_size, action_size)
-        self.actor_target = Actor(state_size, action_size)
+        gpu = torch.cuda.is_available()
+        if(gpu):
+            print('GPU/CUDA works! Happy fast training :)')
+            torch.cuda.current_device()
+            torch.cuda.empty_cache()
+            self.device = torch.device("cuda")
+        else:
+            print('training on cpu...')
+            self.device = torch.device("cpu")
 
+        self.actor = Actor(state_size, action_size).to(self.device)
+        self.actor_target = Actor(state_size, action_size).to(self.device)
         self.actor_optim = optim.Adam(self.actor.parameters(), lr=0.0001)
-        self.critic = Critic(state_size, action_size)
-        self.critic_target = Critic(state_size, action_size)
-        self.critic_optim = optim.Adam(self.critic.parameters(), lr=0.001)
-        self.replay_buffer = deque(maxlen=100000)
-        self.gamma = 0.99
-        self.batch_size = 128
-        self.device = torch.device("cpu")
+        self.critic = Critic(state_size, action_size).to(self.device)
+        self.critic_target = Critic(state_size, action_size).to(self.device)
+        self.critic_optim = optim.Adam(self.critic.parameters(), lr=0.001, weight_decay=0)
+        self.replay_buffer = deque(maxlen=1000000)#1m
+        self.gamma = 0.95#0.99
+        self.batch_size = 128        
         self.tau = 0.001        
         self.seed = random.seed(2)
-        self.noise = OUNoise(action_size, 2)
+        self.noise = OUNoise((20, action_size), 2)
         self.target_network_update(self.actor_target, self.actor, 1.0)
         self.target_network_update(self.critic_target, self.critic, 1.0)
 
-
-#for agnt in range(20):
     def select_actions(self, state):
-        state = torch.from_numpy(state).float()
+        state = torch.from_numpy(state).float().to(self.device)
         self.actor.eval()
         with torch.no_grad():
             actions = self.actor(state).cpu().data.numpy()
@@ -44,10 +50,8 @@ class Agent():
     def add(self, sars):
         self.replay_buffer.append(sars)
 
-    def train(self):
-        if(len(self.replay_buffer) > self.batch_size):
-            self.actor_optim.zero_grad()
-            self.critic_optim.zero_grad()
+    def train(self):    
+        if(len(self.replay_buffer) > self.batch_size): 
             states, actions, rewards, next_states, dones = self.sample()            
             next_actions = self.actor_target(next_states)
             next_state_q_v = self.critic_target(next_states, next_actions)
@@ -55,12 +59,14 @@ class Agent():
             q_targets = rewards + (self.gamma * next_state_q_v * (1-dones))
             current_q_v = self.critic(states, actions)
             critic_loss = F.mse_loss(current_q_v, q_targets)
+            self.critic_optim.zero_grad()
             critic_loss.backward()
             torch.nn.utils.clip_grad_norm(self.critic.parameters(), 1)
             self.critic_optim.step()
 
             actions = self.actor(states)
             actor_loss = -self.critic(states, actions).mean()
+            self.actor_optim.zero_grad()
             actor_loss.backward()
             self.actor_optim.step()
             self.target_network_update(self.actor_target, self.actor, self.tau)
@@ -71,25 +77,18 @@ class Agent():
             target_param.data.copy_(tau * network_param.data + (1.0-tau) * target_param.data)
 
     def sample(self):        
-        samples = []        
-        for i in range(0, self.batch_size):
-            idx = np.random.choice(len(self.replay_buffer))
-            sars = self.replay_buffer[idx]
-            samples.append(sars)        
-        states = torch.tensor([s[0] for s in samples]).float().to(self.device)
+        samples = random.sample(self.replay_buffer, k=self.batch_size)     
+        states = torch.tensor([s[0] for s in samples]).float().to(self.device)        
         actions = torch.tensor([s[1] for s in samples]).float().to(self.device)
-        rewards = torch.tensor([s[2] for s in samples]).float()#.to(self.device)
+        rewards = torch.tensor([s[2] for s in samples]).float().unsqueeze(1).to(self.device)
         next_states = torch.tensor([s[3] for s in samples]).float().to(self.device)
-        dones = torch.tensor([s[4] for s in samples]).float()#.to(self.device)
+        dones = torch.tensor([s[4] for s in samples]).float().unsqueeze(1).to(self.device)
         return states, actions, rewards, next_states, dones
-
-
-
 
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
-    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.1):
+    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.05):#0.1,0.08,0.06
         """Initialize parameters and noise process."""
         self.mu = mu * np.ones(size)
         self.theta = theta
@@ -104,6 +103,6 @@ class OUNoise:
     def sample(self):
         """Update internal state and return it as a noise sample."""
         x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        dx = self.theta * (self.mu - x) + self.sigma * np.array(torch.rand(x.shape))
         self.state = x + dx
         return self.state

networks.py
@@ -12,9 +12,10 @@ class Actor(nn.Module):
     def __init__(self, state_size, action_size):
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(2)
-        self.fc1 = nn.Linear(state_size, 256)        
-        self.fc2 = nn.Linear(256, 256)
+        self.fc1 = nn.Linear(state_size, 128)        
+        self.fc2 = nn.Linear(128, 256)
         self.fc3 = nn.Linear(256, 4)
+        self.batch_norm = nn.BatchNorm1d(128)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -23,7 +24,7 @@ class Actor(nn.Module):
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.batch_norm(self.fc1(x)))
         x = F.relu(self.fc2(x))
         x = F.tanh(self.fc3(x))
         return x
@@ -32,9 +33,10 @@ class Critic(nn.Module):
     def __init__(self, state_size, action_size):
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(2)
-        self.fc1 = nn.Linear(state_size, 256)
-        self.fc2 = nn.Linear(256+4, 256)
+        self.fc1 = nn.Linear(state_size, 128)
+        self.fc2 = nn.Linear(128+4, 256)
         self.fc3 = nn.Linear(256, 1)
+        self.batch_norm = nn.BatchNorm1d(128)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -43,7 +45,7 @@ class Critic(nn.Module):
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, x, actions):
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.batch_norm(self.fc1(x)))
         x = F.relu(self.fc2(torch.cat((x, actions), dim=1)))
         x = self.fc3(x)
         return x

runner.py
@@ -3,6 +3,7 @@ import torch
 from collections import deque
 from agent import Agent
 import numpy as np
+import matplotlib.pyplot as plt
 
 env = UnityEnvironment(file_name='Reacher_20_agents/Reacher.exe')
 brain_name = env.brain_names[0]
@@ -13,34 +14,41 @@ print(observation_state_size)
 print(action_space_size)
 
 training_interval = 20
-train_steps = 10
+train_steps = 1 #10
 agent = Agent(observation_state_size, action_space_size)
-scores = deque(maxlen=100)
-for episode in range(150):
+scores = []
+scores_last_hundred_episodes = deque(maxlen=100)
+for episode in range(300):
     env_info = env.reset(train_mode=True)[brain_name] # reset the environment
     states = env_info.vector_observations #states = env_info.vector_observations[0] 
     agent.reset()
     rewards = 0
-    for timestep in range(300000):
-        #actions = []
-        #for agnt in range(20):
-           # actions.append(agent.select_actions(states[agnt]))
-        actions = agent.select_actions(states)        
+    for timestep in range(1001):
+        actions = agent.select_actions(states)      
         env_info = env.step(actions)[brain_name]        # send the action to the environment
         next_state = env_info.vector_observations  #next_state = env_info.vector_observations[0]
         reward = env_info.rewards                  # same
         done = env_info.local_done                 # same
-        if(timestep % training_interval==0):
-            for _ in range(train_steps):
-                agent.train()
         for agnt in range(20):
             sars = (states[agnt], actions[agnt], reward[agnt], next_state[agnt], done[agnt])
             agent.add(sars)
+            if(timestep % training_interval==0):                
+                for _ in range(train_steps):
+                    agent.train()
+
         states = next_state
         rewards += np.mean(reward)
         if(np.any(done)):
             break
     scores.append(rewards)
-    
+    scores_last_hundred_episodes.append(rewards)
     #if(episode % 100 == 0):
-    print('episode {} rewards {:.2f} mean score {:.2f}'.format(episode, rewards, np.mean(scores)))
+    print('episode {} rewards {:.2f} mean score(100ep) {:.2f}'.format(episode, rewards, np.mean(scores_last_hundred_episodes)))
+torch.save(agent.actor.state_dict(), 'actor_checkpoint.pth')
+torch.save(agent.actor.state_dict(), 'critic_checkpoint.pth')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()